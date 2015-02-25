17bbc5133afff03e773c12d95896b7849202bef1
webedem <webedem@users.noreply.github.com>
2015-02-25 21:50:11 +0200

    Update demo.js

--- dev/demo.js
+++ dev/demo.js
@@ -8,6 +8,13 @@
 	published: '10/01/2015',
 	
 	init: function(params){
+		/*
+		   init is the default constructor. Here you can add your own code or call other functions.
+		   You need to change the namespace (DEMOFUNCTION) to your own namespace (something to avoid confussions with others)
+		   
+		   The only two functions needed for outputing something on the console are $.shellOutput('your message') and $.shellError('your error message')
+		*/
+		
 		// Output on consolio:
 		$.shellOutput('This is a demo function');
 		
